248d406265b7080b7dc14977b852e132e1eb4bef
Daniel Donda <danieldonda@gmail.com>
2023-02-15 11:39:24 -0300

    Cuidado - Somente execute em ambiente de lab.

--- DisableTaskManagerReg.bat
+++ DisableTaskManagerReg.bat
@@ -1,1 +1,4 @@
+@Echo off
+REM O comando abaixo Desabilita o task manager
+REM Somente execute em ambiente de laboratório
 reg.exe add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\ System /v DisableTaskMgr /t REG_DWORD /d 1 /f
